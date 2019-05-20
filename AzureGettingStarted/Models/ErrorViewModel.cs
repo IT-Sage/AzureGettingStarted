12a1f12729501147529c9b59ba5ac761b900ec32
neon333 <ryska.v@gmail.com>
2019-05-20 12:15:34 +0200

    Add project files.

---
+++ AzureGettingStarted/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureGettingStarted.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
